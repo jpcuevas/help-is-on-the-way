8eef119d6df5eebdf0b4b9e80f513a8ed4657a27
JP <xiriusxx@gmail.com>
2016-05-11 09:25:23 -0500

    new date function

--- Package/Body/pkg_util.sql
+++ Package/Body/pkg_util.sql
@@ -1,6 +1,7 @@
-CREATE OR REPLACE
+create or replace 
 PACKAGE BODY PKG_UTIL AS
-
+------------------------------
+-----------------------------
   function fn_numtowords(
   v_num IN NUMBER
 )
@@ -13,5 +14,64 @@ BEGIN
   SELECT TO_CHAR (TO_DATE (v_num, 'j'), 'jsp') INTO v_numberwords  FROM DUAL;
   RETURN v_numberwords;
   END fn_numtowords;
-
+------------------------------
+----------------------------- 
+  function firstDtofMonth 
+  
+  Return date
+  As
+    v_date date;
+    
+  begin
+  SELECT TRUNC (SYSDATE, 'MONTH') "First day of current month"
+   into v_date FROM DUAL;
+   
+   return v_date;
+  end firstDtofMonth;
+------------------------------
+-----------------------------
+ function lastDtofMonth 
+  
+  Return date
+  As
+    v_date date;
+    
+  begin
+  SELECT TRUNC (LAST_DAY (SYSDATE)) "Last day of current month"
+   into v_date FROM DUAL;
+   
+   return v_date;
+  end lastDtofMonth;
+  
+  ------------------------------
+-----------------------------
+ function firstDtofYear 
+  
+  Return date
+  As
+    v_date date;
+    
+  begin
+  SELECT TRUNC (SYSDATE, 'YEAR') "Year First Day"
+   into v_date FROM DUAL;
+   
+   return v_date;
+  end firstDtofYear;
+  
+------------------------------
+----------------------------- 
+  
+  function lastDtofYear 
+  
+  Return date
+  As
+    v_date date;
+    
+  begin
+  SELECT ADD_MONTHS (TRUNC (SYSDATE, 'YEAR'), 12) - 1 "Year Last Day" 
+   into v_date FROM DUAL;
+   
+   return v_date;
+  end lastDtofYear;
+  
 END PKG_UTIL;
--- Package/pkg_util.sql
+++ Package/pkg_util.sql
@@ -1,7 +1,22 @@
 create or replace 
 PACKAGE PKG_UTIL AS 
-
-  /* Convert number to words */
+  /*  serveroutput on size 30000;*/
+  /* convert number to words */
   function fn_numtowords(v_num number) return varchar2;
+  
+  /* return first day of month */
+  function firstDtofMonth return date;
+  
+  /* return first day of month */
+  function lastDtofMonth return date;
+  
+  /* return first day of year */
+  function firstDtofYear return date;
+  
+  /*Get the last day of the year*/
+  function lastDtofYear return date;
+  
+  /*Get number of days in current month*/
+  function currentday(v_date date) return date;
 
 END PKG_UTIL;
